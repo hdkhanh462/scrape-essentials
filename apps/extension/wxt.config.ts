0fe12597f711c2d2139f2f5d8777b26977f3a2ae
Hoàng Dương Khánh <101619460+hdkhanh462@users.noreply.github.com>
2026-04-05 17:26:01 +0700

    feat: update versions to `0.1.6`

--- apps/extension/wxt.config.ts
+++ apps/extension/wxt.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   manifest: () => ({
     name: "Scrape Essentials",
     description: "A browser extension for web scraping and automation.",
-    version: "0.1.5",
+    version: "0.1.6",
     permissions: ["activeTab", "identity", "storage", "scripting", "alarms"],
     host_permissions: [`${import.meta.env.VITE_API_URL}/*`],
     oauth2: {
