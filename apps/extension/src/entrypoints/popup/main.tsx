80010685ad58893e83a0cc9ef5b56347e7e7bb79
Hoàng Dương Khánh <101619460+hdkhanh462@users.noreply.github.com>
2026-04-27 20:52:47 +0700

    feat(i18n): add internationalization support with English and Vietnamese configurations
    - Created I18nProvider component to manage language settings based on user preferences.
    - Added English and Vietnamese i18n configuration files with respective message resources.
    - Introduced JSON files for English and Vietnamese translations.

--- apps/extension/src/entrypoints/popup/main.tsx
+++ apps/extension/src/entrypoints/popup/main.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import "@/global.css";
+import "@/i18n/config.en";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import App from "@/entrypoints/popup/App";
 import { queryClient } from "@/features/shared/query-client";
-import "@/global.css";
-import { QueryClientProvider } from "@tanstack/react-query";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
